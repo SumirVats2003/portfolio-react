947435da3a8a8a08923650f861e034ee2ba7b6f5
Sumir Vats <sumirvats@gmail.com>
2023-02-20 18:13:33 +0530

    Added achievement cards on About Page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Card from "./Card";
+import { Link } from "react-router-dom";
 import "../App.css";
 
 function About() {
@@ -7,9 +9,37 @@ function About() {
     <>
       <Navbar />
       <h1>About Me</h1>
-      <div className="intro">
-        Here are the links to my social media accounts. Feel free to contact
-        anytime as per your ease
+      <div className="about">
+        I am a Computer Engineering undergraduate from the Zakir Husain College
+        of Engineering and Technology, Aligarh Muslim University. I am
+        passionate about software development and have worked on several
+        projects involving web development, application development, and other
+        software-related solutions. I am an Open Source Enthusiast and am now
+        leading the&nbsp;
+        <Link
+          to="https://gdsc.community.dev/zakir-husain-college-of-engineering-and-technology-aligarh/"
+          target="_blank"
+        >
+          Google Developer Student Club, ZHCET
+        </Link>
+      </div>
+      <div className="achievments">
+        <h2>Achievements</h2>
+        <div className="cards">
+          <Card
+            head="Card"
+            body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates facilis impedit dicta provident aspernatur ipsa, neque et delectus magni unde culpa natus laudantium molestias atque perspiciatis rem dolor nisi, mollitia velit ex pariatur fuga. Consequatur!"
+            showBtn={true}
+          />
+          <Card
+            head="Card"
+            body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates facilis impedit dicta provident aspernatur ipsa, neque et delectus magni unde culpa natus laudantium molestias atque perspiciatis rem dolor nisi, mollitia velit ex pariatur fuga. Consequatur!"
+          />
+          <Card
+            head="Card"
+            body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates facilis impedit dicta provident aspernatur ipsa, neque et delectus magni unde culpa natus laudantium molestias atque perspiciatis rem dolor nisi, mollitia velit ex pariatur fuga. Consequatur!"
+          />
+        </div>
       </div>
     </>
   );
